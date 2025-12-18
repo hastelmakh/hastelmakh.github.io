d2086e811a34c21025b00e714dd7917ab69bbbc6
Volodymyr Stelmakh <volod.stelmakh@gmail.com>
2025-12-18 16:31:47 +0100

    add next and previous projects links

--- src/Projects/ProjectController.php
+++ src/Projects/ProjectController.php
@@ -14,13 +14,22 @@ class ProjectController implements ControllerInterface
 
     public function __construct(
         private readonly Environment $twig,
+        private readonly ProjectRepository $projectRepository,
     ) {}
 
     public function render(Router $router, array $parameters): string
     {
         /** @var Project $project */
         $project = $parameters[self::PARAM_PROJECT];
+
+        $previous = $this->projectRepository->previous($project);
+        $next = $this->projectRepository->next($project);
+
         $template = sprintf('@Projects~%s/details.html.twig', $project->slug);
-        return $this->twig->render($template, ['project' => $project]);
+        return $this->twig->render($template, [
+            'project' => $project,
+            'previous' => $previous,
+            'next' => $next,
+        ]);
     }
 }
--- src/Projects/ProjectRepository.php
+++ src/Projects/ProjectRepository.php
@@ -6,12 +6,42 @@ namespace App\Projects;
 
 class ProjectRepository
 {
-    /** @var ?array<Project> */
+    /** @var ?list<Project> */
     private ?array $projects = null;
 
+    /** @var array<int, int> */
+    private array $indexMap = [];
+
+    /**
+     * @return list<Project>
+     */
     public function all(): array
     {
         return $this->getProjects();
+    }
+
+    public function next(Project $project): ?Project
+    {
+        $id = $this->getProjectId($project);
+        $index = $this->indexMap[$id] ?? null;
+
+        if ($index === null) {
+            return null;
+        }
+
+        return $this->projects[$index + 1] ?? $this->projects[0];
+    }
+
+    public function previous(Project $project): ?Project
+    {
+        $id = $this->getProjectId($project);
+        $index = $this->indexMap[$id] ?? null;
+
+        if ($index === null) {
+            return null;
+        }
+
+        return $this->projects[$index - 1] ?? $this->projects[count($this->projects) - 1];
     }
 
     private function getProjects(): array
@@ -19,13 +49,14 @@ class ProjectRepository
         if ($this->projects === null) {
             $projects = $this->collectProjectsFromResources();
             $this->projects = $this->sortProjectsByPriority($projects);
+            $this->indexMap = $this->buildIndexMap($this->projects);
         }
 
         return $this->projects;
     }
 
     /**
-     * @return array<Project>
+     * @return list<Project>
      */
     private function collectProjectsFromResources(): array
     {
@@ -50,8 +81,8 @@ class ProjectRepository
     }
 
     /**
-     * @param array<Project> $projects
-     * @return array<Project>
+     * @param list<Project> $projects
+     * @return list<Project>
      */
     private function sortProjectsByPriority(array $projects): array
     {
@@ -60,5 +91,26 @@ class ProjectRepository
         });
 
         return $projects;
+    }
+
+    /**
+     * @param list<Project> $projects
+     * @return array<int, int>
+     */
+    private function buildIndexMap(array $projects): array
+    {
+        $result = [];
+
+        foreach ($projects as $index => $project) {
+            $id = $this->getProjectId($project);
+            $result[$id] = $index;
+        }
+
+        return $result;
+    }
+
+    private function getProjectId(Project $project): int
+    {
+        return spl_object_id($project);
     }
 }
