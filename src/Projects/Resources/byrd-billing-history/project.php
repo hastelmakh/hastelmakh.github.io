da8f34fa003b8c9d9d0b5053c84dc4d65bbefae7
Volodymyr Stelmakh <volod.stelmakh@gmail.com>
2025-12-25 14:17:52 +0100

    unify byrd projects naming

--- src/Projects/Resources/byrd-billing-history/project.php
+++ src/Projects/Resources/byrd-billing-history/project.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 return new \App\Projects\Project(
     basename(__DIR__),
     30,
-    'Billing History Management',
+    'Byrd Billing History',
     'A solution for customers to manage their payment history independently',
 );
--- src/Projects/Resources/byrd-design-system/project.php
+++ src/Projects/Resources/byrd-design-system/project.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 return new \App\Projects\Project(
     basename(__DIR__),
     10,
-    'Design System',
+    'Byrd Design System',
     'The project offers tools and guidelines for consistent, scalable design',
 );
